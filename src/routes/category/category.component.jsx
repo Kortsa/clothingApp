cbcae33c3d1ad13f7b0cf60f16aaab38288d5867
kortsa <leoklomegah8@gmail.com>
2024-03-22 22:37:22 -0400

    styling

--- src/routes/category/category.component.jsx
+++ src/routes/category/category.component.jsx
@@ -8,7 +8,7 @@ import './category.styles.scss';
 const Category = () => {
     const { category } = useParams();
     const { categoriesMap } = useContext(CategoriesContext);
-    const [products, setProducts] = useState([]);
+    const [products, setProducts] = useState(categoriesMap[category]);
 
     useEffect(() => {
         setProducts(categoriesMap[category]);
@@ -16,12 +16,11 @@ const Category = () => {
 
     return(
         <div className='category-container'>
-            {
+            {products &&
                 products.map((product) => <ProductCard key={product.id} product={product} />)
             }
         </div>
     )
-
 }
 
 export default  Category;
